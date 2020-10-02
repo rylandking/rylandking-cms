397e0ab11b3ac49a762ea7a3c0f2a43541557495
rylandking <ryland@users.noreply.github.com>
2020-10-02 11:52:08 -0600

    update preview url to rylandking.com

--- resolveProductionUrl.js
+++ resolveProductionUrl.js
@@ -1,5 +1,5 @@
 const previewSecret = 'Ifuckinglovesanity69!'; // Copy the string you used for SANITY_PREVIEW_SECRET in env.local
-const projectUrl = 'http://localhost:3000';
+const projectUrl = 'http://rylandking.com';
 const sanityClient = require('@sanity/client');
 
 export default function resolveProductionUrl(document) {
